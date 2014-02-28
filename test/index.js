8ed0223cec680a201210b0ea08b14eb217fde9bb
Michael Williams <dinosaur@riseup.net>
2014-02-27 17:12:37 -0800

    add another file split line test but using baconjs to split

--- test/index.js
+++ test/index.js
@@ -30,6 +30,26 @@ describe('#Bacon.fromNodeStream', function () {
 
   it("should work for file stream split on lines", function (done) {
     var nodeStream =
+      fs.createReadStream(__dirname + '/file.txt', { encoding: "utf8" });
+
+    var eventStream = Bacon.fromNodeStream(nodeStream)
+    .flatMap(function (str) {
+      return Bacon.fromArray(str.split('\n'));
+    })
+    var lines = collect(eventStream);
+    eventStream.onValue(function () {
+      if (lines.length === 3) {
+        expect(lines[0]).to.equal("i am a test file");
+        expect(lines[1]).to.equal("hear me roar!");
+        expect(lines[2]).to.equal("hehehehe. :)");
+        done()
+        return Bacon.noMore;
+      }
+    });
+  });
+
+  it("should work for line stream", function (done) {
+    var nodeStream =
       fs.createReadStream(__dirname + '/file.txt', { encoding: "utf8" })
       .pipe(split());
 
